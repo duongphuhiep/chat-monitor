0a53a30ea683066405c738a9a50a62872b6b2d76
Phu-Hiep DUONG <duongphuhiep@gmail.com>
2025-04-16 01:16:15 +0200

    add tailwind

--- webapp/src/routes/login.tsx
+++ webapp/src/routes/login.tsx
@@ -1,42 +1,167 @@
-import {
-  useSubmission,
-  type RouteSectionProps
-} from "@solidjs/router";
+import { createSignal } from "solid-js";
+import { useSubmission, type RouteSectionProps } from "@solidjs/router";
 import { Show } from "solid-js";
 import { loginOrRegister } from "~/lib";
 
 export default function Login(props: RouteSectionProps) {
   const loggingIn = useSubmission(loginOrRegister);
+  const [isLogin, setIsLogin] = createSignal(true); // Signal to track the selected form
 
   return (
-    <main>
-      <h1>Login</h1>
-      <form action={loginOrRegister} method="post">
-        <input type="hidden" name="redirectTo" value={props.params.redirectTo ?? "/"} />
-        <fieldset>
-          <legend>Login or Register?</legend>
-          <label>
-            <input type="radio" name="loginType" value="login" checked={true} /> Login
-          </label>
-          <label>
-            <input type="radio" name="loginType" value="register" /> Register
-          </label>
-        </fieldset>
-        <div>
-          <label for="username-input">Username</label>
-          <input name="username" placeholder="kody" />
-        </div>
-        <div>
-          <label for="password-input">Password</label>
-          <input name="password" type="password" placeholder="twixrox" />
-        </div>
-        <button type="submit">Login</button>
-        <Show when={loggingIn.result}>
-          <p style={{color: "red"}} role="alert" id="error-message">
-            {loggingIn.result!.message}
-          </p>
-        </Show>
-      </form>
+    <main class="min-h-screen flex items-center justify-center bg-gray-100">
+      <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        <h1 class="text-2xl font-bold mb-6 text-center text-gray-800">Login</h1>
+        <form action={loginOrRegister} method="post" class="space-y-6">
+          <input
+            type="hidden"
+            name="redirectTo"
+            value={props.params.redirectTo ?? "/"}
+          />
+          <fieldset class="space-y-2">
+            <legend class="font-medium text-gray-700 mb-2">
+              Login or Register?
+            </legend>
+            <div class="flex gap-4">
+              <label class="flex items-center space-x-2">
+                <input
+                  type="radio"
+                  name="loginType"
+                  value="login"
+                  checked={isLogin()}
+                  class="text-blue-600"
+                  onInput={() => setIsLogin(true)} // Update signal on change
+                />
+                <span>Login</span>
+              </label>
+              <label class="flex items-center space-x-2">
+                <input
+                  type="radio"
+                  name="loginType"
+                  value="register"
+                  class="text-blue-600"
+                  onInput={() => setIsLogin(false)} // Update signal on change
+                />
+                <span>Register</span>
+              </label>
+            </div>
+          </fieldset>
+
+          {/* ************ login form *********** */}
+          <Show when={isLogin()}>
+            <div id="login-form" class="space-y-6">
+              <div class="space-y-1">
+                <label
+                  for="username-input"
+                  class="block text-sm font-medium text-gray-700"
+                >
+                  Username
+                </label>
+                <input
+                  id="username-input"
+                  name="username"
+                  placeholder="kody"
+                  class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+              <div class="space-y-1">
+                <label
+                  for="password-input"
+                  class="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <input
+                  id="password-input"
+                  name="password"
+                  type="password"
+                  placeholder="twixrox"
+                  class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+              <button
+                type="submit"
+                class="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+              >
+                Login
+              </button>
+              <Show when={loggingIn.result}>
+                <p
+                  class="text-red-600 text-sm"
+                  role="alert"
+                  id="error-message"
+                >
+                  {loggingIn.result!.message}
+                </p>
+              </Show>
+            </div>
+          </Show>
+
+          {/* ************ register form *********** */}
+          <Show when={!isLogin()}>
+            <div id="register-form" class="space-y-6">
+              <div class="space-y-1">
+                <label
+                  for="username-input"
+                  class="block text-sm font-medium text-gray-700"
+                >
+                  Username
+                </label>
+                <input
+                  id="username-input"
+                  name="username"
+                  placeholder="kody"
+                  class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+              <div class="space-y-1">
+                <label
+                  for="password-input"
+                  class="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <input
+                  id="password-input"
+                  name="password"
+                  type="password"
+                  placeholder="twixrox"
+                  class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+              <div class="space-y-1">
+                <label
+                  for="confirm-password-input"
+                  class="block text-sm font-medium text-gray-700"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  id="confirm-password-input"
+                  name="confirm-password"
+                  type="password"
+                  placeholder="twixrox"
+                  class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+              <button
+                type="submit"
+                class="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+              >
+                Register
+              </button>
+              <Show when={loggingIn.result}>
+                <p
+                  class="text-red-600 text-sm"
+                  role="alert"
+                  id="error-message"
+                >
+                  {loggingIn.result!.message}
+                </p>
+              </Show>
+            </div>
+          </Show>
+        </form>
+      </div>
     </main>
   );
 }
