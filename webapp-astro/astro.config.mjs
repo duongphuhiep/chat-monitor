59ec3518377de5326019f036795d3cb6d125d76e
Phu-Hiep DUONG <duongphuhiep@gmail.com>
2025-05-08 23:19:39 +0200

    node adapter

--- webapp-astro/astro.config.mjs
+++ webapp-astro/astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import alpinejs from '@astrojs/alpinejs';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -12,4 +14,8 @@ export default defineConfig({
   },
 
   integrations: [alpinejs()],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
